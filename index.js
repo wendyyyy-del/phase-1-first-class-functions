d7cccdaa2afb53b95eb0f0b12a917bf0c78df273
sarah <sarah.akelo@student.moringaschool.com>
2025-03-12 08:43:18 +0300

    Complete

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  function returnsANamedFunction(){
+    function namedFunction() {}
+        return namedFunction;
+    
+      }
+
+      function returnsAnAnonymousFunction() {
+        return function() {}
+      }
